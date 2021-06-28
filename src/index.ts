4fced1e747060501d382e322c897f7b594735c71
Julian <JulianDM1995@gmail.com>
2021-06-28 17:46:33 -0500

    create functions to getFrecuencyByString and getByweekdayByString

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,64 @@
-import { RRule, RRuleSet, rrulestr, Weekday } from 'rrule'
-
-// Create a rule:
-const rule = new RRule({
-    freq: RRule.WEEKLY,
-    interval: 5,
-    byweekday: [RRule.MO, RRule.FR],
-    dtstart: new Date(Date.UTC(2012, 1, 1, 10, 30)),
-    until: new Date(Date.UTC(2012, 12, 31))
-})
-
-// Get all occurrence dates (Date instances):
-//console.log(rule.all())
-
+import { Frequency, RRule, Weekday } from 'rrule'
 
 test()
+
+function getFrecuencyByString(frecuencyStr: string): Frequency {
+    switch (frecuencyStr) {
+        case "YEARLY": return RRule["YEARLY"]
+        case "MONTHLY": return RRule["MONTHLY"]
+        case "WEEKLY": return RRule["WEEKLY"]
+        case "DAILY": return RRule["DAILY"]
+        case "HOURLY": return RRule["HOURLY"]
+        case "MINUTELY": return RRule["MINUTELY"]
+        default: return RRule["SECONDLY"]
+    }
+}
+
+function getByweekdayByStringArr(weekdays: string[]): Weekday[] {
+    var weekdaysReturn: Weekday[] = []
+    weekdays.forEach(day => {
+        switch (day) {
+            case "MONDAY": weekdaysReturn.push(RRule.MO); break;
+            case "TUESDAY": weekdaysReturn.push(RRule.TU); break;
+            case "WEDNESDAY": weekdaysReturn.push(RRule.WE); break;
+            case "THURSDAY": weekdaysReturn.push(RRule.TH); break;
+            case "FRIDAY":weekdaysReturn.push(RRule.FR); break;
+            case "SATURDAY": weekdaysReturn.push(RRule.SA); break;
+            default: return weekdaysReturn.push(RRule.SU);
+        }
+    });
+    return weekdaysReturn;
+}
+
 function test() {
 
+    const freqStr: string = "WEEKLY"
+    const Freq: Frequency = getFrecuencyByString(freqStr);
 
-    const freq = "WEEKLY"
-    console.log(RRule[freq])
+    const Interval: number = 5;
 
+    const byweekdayStr: string[] = ["MONDAY", "FRIDAY"];
+    const Byweekday: Weekday[] = getByweekdayByStringArr(byweekdayStr);
 
-    const byweekday =
-        [
-            'MONDAY',
-            'TUESDAY',
-            'FRIDAY',
-        ]
+    const startStr: string = "2012-02-01T10:30:00.000Z"
+    const Start = new Date(startStr);
 
+    const endStr: string = "2013-01-31T00:00:00.000Z"
+    const End = new Date(endStr);
+
+    const rule = new RRule({
+        freq: Freq,
+        interval: Interval,
+        byweekday: Byweekday,
+        dtstart: Start,
+        until: End
+    })
+
+    console.log(rule.all())
+
+}
+
+function test2() {
     const daysHash = {
         MONDAY: new Weekday(0),
         TUESDAY: new Weekday(1),
@@ -38,12 +69,5 @@ function test() {
         SUNDAY: new Weekday(6),
     }
 
-    console.log(daysHash[0])
-
-/*
-    console.log(
-        byweekday.map(day => {
-            return daysHash[day]
-        })
-    )*/
+    console.log(daysHash["MONDAY"])
 }
